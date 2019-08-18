5802c0639d71a24c6eb4b41a5f531edb14dfc46b
casperklein <casperklein@users.noreply.github.com>
2019-08-19 00:15:28 +0200

    modified:   docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -2,13 +2,19 @@
 
 echo "sftp:$password" | chpasswd
 
-cd /etc/ssh
+cd /etc/ssh || exit
 
 rm -f ssh_host_ecdsa_key ssh_host_rsa_key ssh_host_ed25519_key
 
+echo "Creating SSH2 ECDSA key; this may take some time.."
 ssh-keygen -q -f ssh_host_ecdsa_key   -N '' -t ecdsa   -b 521  # Max key size for ecdsa is 521
+
+echo "Creating SSH2 RSA key; this may take some time.."
 ssh-keygen -q -f ssh_host_rsa_key     -N '' -t rsa     -b 2048 # 2048 bits is considered sufficient.
+
+echo "Creating SSH2 ED25519 key; this may take some time.."
 ssh-keygen -q -f ssh_host_ed25519_key -N '' -t ed25519         # Ed25519 keys have a fixed length and the -b flag will be ignored.
+echo
 
 echo "Fingerprints:"
 echo
